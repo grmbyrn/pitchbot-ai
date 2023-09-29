fdcfc45e636e21976c41a79f1032c2b33b4005f8
grmbyrn <graemebyrne_89@hotmail.com>
2023-09-29 13:01:52 +0200

    create functions to add and show synopsis, title and actors for movie using openai api

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { process } from '/env'
 import { Configuration, OpenAIApi } from 'openai'
 
-const setupTextarea = document.getElementById('setup-textarea')
 const setupInputContainer = document.getElementById('setup-input-container')
 const movieBossText = document.getElementById('movie-boss-text')
 
@@ -12,20 +11,81 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 document.getElementById("send-btn").addEventListener("click", () => {
+  const setupTextarea = document.getElementById('setup-textarea')
   if (setupTextarea.value) {
     const userInput = setupTextarea.value
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
     fetchBotReply(userInput)
+    fetchSynopsis(userInput)
   }
 })
 
 async function fetchBotReply(outline) {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `Generate a short message to enthusiastically say "${outline}" sounds interesting and that you need some minutes to think about it. Mention one aspect of the sentence.`,
+    prompt: `Generate a short message to enthusiastically say "${outline}" sounds interesting and that you need some minutes to think about it.
+    ###
+    outline: A doctor discovers a time-travel device but faces unintended consequences and must stop a shadowy group from altering history
+    message: "Wow, the idea of a doctor discovering a time-travel device and facing unintended consequences has me intrigued! Need a moment to ponder."
+    ###
+    outline: ${outline}
+    message:
+    `,
     max_tokens: 60
   })
   movieBossText.innerText = response.data.choices[0].text.trim()
   console.log(response)
+}
+
+async function fetchSynopsis(outline){
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline. The synopsis should include actors names in brackets after each character. Choose actors that would be ideal for this role. 
+    ###
+    outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+    ###
+    outline: ${outline}
+    synopsis:
+    ###`,
+    max_tokens: 700
+  })
+  const synopsis = response.data.choices[0].text.trim()
+  document.getElementById('output-text').innerText = synopsis
+  fetchTitle(synopsis)
+  fetchStars(synopsis)
+}
+
+async function fetchTitle(synopsis){
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `Generate an interesting, intriguing but short and snappy title for a movie based on the ${synopsis}
+    ###
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+    title: Skyward Challenge: Maverick's Redemption
+    ###
+    synopsis: ${synopsis}
+    title:
+    `,
+    max_tokens: 25,
+    temperature: 0.7
+  })
+  document.getElementById('output-title').innerText = response.data.choices[0].text.trim()
+}
+
+async function fetchStars(synopsis){
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `Generate the names of three actors who would suit the primary acting roles in this movie based on the ${synopsis}. Add 'and' before the final actor's name.
+    ###
+    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+    actors: Tom Cruise, Val Kilmer, Kelly McGillis
+    ###
+    synopsis: ${synopsis}
+    actors:
+    `,
+    max_tokens: 30
+  })
+  document.getElementById('output-stars').innerText = response.data.choices[0].text.trim()
 }
